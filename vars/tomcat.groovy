f5d31c4d925e4fc4c01da8c74b81017327f75a6f
sri <srikanth@gmail.com>
2023-07-04 17:30:14 +0000

    added tomcat groovy

---
+++ vars/tomcat.groovy
@@ -0,0 +1,10 @@
+
+def call(env,war,ip){
+
+sshagent(['env']) {
+sh 'scp  -o StrictHostKeyChecking=no target/war ec2-user@ip:/opt/tomcat9/webapps'
+sh 'ssh ec2-user@ip /opt/tomcat9/bin/shutdown.sh'
+sh 'ssh ec2-user@ip /opt/tomcat9/bin/startup.sh'
+}
+}
+
