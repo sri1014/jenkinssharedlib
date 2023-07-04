65cb5c62d26748cf95811220763688e2dc3ba89a
sri1014 <86741705+sri1014@users.noreply.github.com>
2023-07-04 23:11:04 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -2,9 +2,9 @@
 def call(env,war,ip){
 
 sshagent(['env']) {
-sh 'scp  -o StrictHostKeyChecking=no target/war ec2-user@ip:/opt/tomcat9/webapps'
-sh 'ssh ec2-user@ip /opt/tomcat9/bin/shutdown.sh'
-sh 'ssh ec2-user@ip /opt/tomcat9/bin/startup.sh'
+sh "scp  -o StrictHostKeyChecking=no target/${war} ec2-user@ip:/opt/tomcat9/webapps"
+sh "ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"
+sh "ssh ec2-user@${ip} /opt/tomcat9/bin/startup.sh"
 }
 }
 
